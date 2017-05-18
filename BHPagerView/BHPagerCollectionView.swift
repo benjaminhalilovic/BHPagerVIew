c563fca01352297814737de739da9915e379ec61
mac <mac@mac.local>
2017-05-18 15:09:04 +0200

    Initial Commit

---
+++ BHPagerView/BHPagerCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  BHPagerCollectionView.swift
+//  BHPagerView
+//
+//  Created by mac on 18/05/17.
+//  Copyright © 2017 Benjamin Halilovic. All rights reserved.
+//
+
+import UIKit
+
+class BHPagerCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ BHPagerView/BHPagerViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BHPagerViewCell.swift
+//  BHPagerView
+//
+//  Created by mac on 18/05/17.
+//  Copyright © 2017 Benjamin Halilovic. All rights reserved.
+//
+
+import UIKit
+
+open class BHPagerViewCell: UICollectionViewCell {
+    
+}
